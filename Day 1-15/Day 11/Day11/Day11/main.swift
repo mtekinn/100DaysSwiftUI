7e96215208ac0d04973e2f01aba249ca5803f849
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-08 00:55:57 +0300

    DAY 11 Structs, part two

---
+++ Day 1-15/Day 11/Day11/Day11/main.swift
@@ -0,0 +1,139 @@
+//
+//  main.swift
+//  Day11
+//
+//  Created by Mehmet Tekin on 7.03.2024.
+//
+
+import Foundation
+
+//struct BankAccount {
+//    private var funds = 0
+//    
+//    mutating func deposit(amount: Int) {
+//        funds += amount
+//    }
+//    
+//    mutating func withdraw(amount: Int) -> Bool {
+//        if funds >= amount {
+//            funds -= amount
+//            return true
+//        } else {
+//            return false
+//        }
+//    }
+//}
+//
+//
+//var account = BankAccount()
+//account.deposit(amount: 100)
+//let success = account.withdraw(amount: 200)
+//
+//if success {
+//    print("Withdraw money successfully")
+//} else {
+//    print("Failed to get the money")
+//}
+
+//struct Doctor {
+//    var name: String
+//    var location: String
+//    private var currentPatient = "No one"
+//    
+//    init(name: String, location: String, currentPatient: String = "No one") {
+//        self.name = name
+//        self.location = location
+//        self.currentPatient = currentPatient
+//    }
+//}
+//
+//// let drJones = Doctor(name: "Esther Jones", location: "Bristol")
+//let dr = Doctor(name: "test", location: "test", currentPatient: "test")
+
+
+//struct Office {
+//    private var passCode: String
+//    var address: String
+//    var employees: [String]
+//    init(address: String, employees: [String]) {
+//        self.address = address
+//        self.employees = employees
+//        self.passCode = "SEKRIT"
+//    }
+//}
+//let monmouthStreet = Office(address: "30 Monmouth St", employees: ["Paul Hudson"])
+
+//struct School {
+//    var staffNames: [String]
+//    private var studentNames: [String]
+//    init(staff: [String]) {
+//        self.staffNames = staff
+//        self.studentNames = [String]()
+//    }
+//}
+//let royalHigh = School(staff: ["Mrs Hughes"])
+
+
+//struct Toy {
+//    var customerPrice: Double
+//    private var actualPrice: Int
+//    init(price: Int) {
+//        actualPrice = price
+//        customerPrice = Double(actualPrice * 2)
+//    }
+//}
+//let buzz = Toy(price: 10)
+
+
+//struct App {
+//    var name: String
+//    private var sales = 0
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+//let spotify = App(name: "Spotify")
+//print(spotify)
+
+//struct School {
+//    static var studentCount = 0
+//    
+//    static func add(student: String) {
+//        print("\(student) joined the school.")
+//        studentCount += 1
+//    }
+//}
+//
+//var s1 = School()
+//// s1
+//School.add(student: "Mehmet Tekin")
+//print(School.studentCount)
+
+
+
+//struct AppData {
+//    static let version = "1.3 beta 2"
+//    static let saveFilename = "settings.json"
+//    static let homeURL = "https://www.hackingwithswift.com"
+//}
+//
+//
+//struct Employee {
+//    let username: String
+//    let password: String
+//    
+//    static let example = Employee(username: "cfederighi", password: "hairforceone")
+//}
+
+
+struct Unwrap {
+    static let appURL = "https://itunes.apple.com/app/id1440611372"
+}
+
+
+var entropy = Int.random(in: 1...1000)
+
+func getEntropy() -> Int {
+    entropy += 1
+    return entropy
+}
