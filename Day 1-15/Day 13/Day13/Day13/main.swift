5d7862c37a11b0cac210f82378a6818da0994f9f
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-10 17:25:37 +0300

    use protocols, opaque return types, extensions and protocol extensions

--- Day 1-15/Day 13/Day13/Day13/main.swift
+++ Day 1-15/Day 13/Day13/Day13/main.swift
@@ -76,36 +76,171 @@ import Foundation
 //getTravelEstimates(using: [car, bike], distance: 150)
 
 
-protocol Purchaseable {
-    var name: String { get set }
-}
+//protocol Purchaseable {
+//    var name: String { get set }
+//}
+//
+//
+//struct Book: Purchaseable {
+//    var name: String
+//    var author: String
+//}
+//
+//struct Movie: Purchaseable {
+//    var name: String
+//    var actors: [String]
+//}
+//
+//struct Car: Purchaseable {
+//    var name: String
+//    var manufacturer: String
+//}
+//
+//struct Coffee: Purchaseable {
+//    var name: String
+//    var strength: Int
+//}
+//
+//
+//func buy(_ item: Purchaseable) {
+//    print("I'm buying \(item.name)")
+//}
+//
+//
+//var b = Book(name: "Test Book", author: "Mehmet Tekin")
+//buy(b)
 
 
-struct Book: Purchaseable {
-    var name: String
-    var author: String
-}
-
-struct Movie: Purchaseable {
-    var name: String
-    var actors: [String]
-}
-
-struct Car: Purchaseable {
-    var name: String
-    var manufacturer: String
-}
-
-struct Coffee: Purchaseable {
-    var name: String
-    var strength: Int
-}
+//func getRandomNumber() -> some Equatable {
+//    Int.random(in: 1...6)
+//}
+//
+//func getRandomBool() -> some Equatable {
+//    Bool.random()
+//}
+//
+//print(getRandomNumber() == getRandomNumber())
 
 
-func buy(_ item: Purchaseable) {
-    print("I'm buying \(item.name)")
-}
+//var quote = "   The truth is rarely pure and never simple   "
+//let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+//
+//extension String {
+//    
+//    func trimmed() -> String {
+//        self.trimmingCharacters(in: .whitespacesAndNewlines)
+//    }
+//    
+//    mutating func trim() {
+//        self = self.trimmed()
+//    }
+//    
+//    var lines: [String] {
+//        self.components(separatedBy: .newlines)
+//    }
+//}
+//
+////let trimmed = quote.trimmed()
+////print(trimmed)
+//
+//func trim(_ string: String) -> String {
+//    string.trimmingCharacters(in: .whitespacesAndNewlines)
+//}
+//
+//let trimmed2 = trim(quote)
+//// print(trimmed2)
+//
+//let lyrics = """
+//But I keep cruising
+//Can't stop, won't stop moving
+//It's like I got this music in my mind
+//Saying it's gonna be alright
+//"""
+//
+//print(lyrics.lines.count)
 
 
-var b = Book(name: "Test Book", author: "Mehmet Tekin")
-buy(b)
+//struct Book {
+//    let title: String
+//    let pageCount: Int
+//    let readingHours: Int
+//    
+//    init(title: String, pageCount: Int, readingHours: Int) {
+//        self.title = title
+//        self.pageCount = pageCount
+//        self.readingHours = pageCount / 50
+//    }
+//}
+//
+//extension Book {
+//    init(title: String, pageCount: Int) {
+//        self.title = title
+//        self.pageCount = pageCount
+//        self.readingHours = pageCount / 50
+//    }
+//}
+//
+//let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
+//print(lotr)
+
+
+//let guests = ["Mario", "Luigi", "Peach"]
+//
+//if guests.isEmpty == false {
+//    print("Guest count: \(guests.count)")
+//}
+//
+//if !guests.isEmpty {
+//    print("Guest count2: \(guests.count)")
+//}
+//
+//
+//extension Array {
+//    var isNotEmpty: Bool {
+//        isEmpty == false
+//    }
+//}
+//
+//if guests.isNotEmpty {
+//    print("Guest count3: \(guests.count)")
+//}
+//
+//extension Collection {
+//    var isNotEmpty: Bool {
+//        isEmpty == false
+//    }
+//}
+//
+//
+//protocol Person {
+//    var name: String { get }
+//    func sayHello()
+//}
+//
+//extension Person {
+//    func sayHello() {
+//        print("Hi, I'm \(name)")
+//    }
+//}
+//
+//
+//struct Employee: Person {
+//    let name: String
+//}
+//
+//let taylor = Employee(name: "Taylor Swift")
+//taylor.sayHello()
+
+
+//let numbers = [4, 8, 15, 16]
+//let allEven = numbers.allSatisfy { $0.isMultiple(of: 2) }
+//
+//print(allEven)
+//
+//let numbers2 = Set([4, 8, 15, 16])
+//let allEven2 = numbers2.allSatisfy { $0.isMultiple(of: 2) }
+//print(allEven2)
+//
+//let numbers3 = ["four": 4, "eight": 8, "fifteen": 15, "sixteen": 16]
+//let allEven3 = numbers3.allSatisfy { $0.value.isMultiple(of: 2) }
+//print(allEven3)
