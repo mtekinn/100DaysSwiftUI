38ca2a2bf438a4ee32ed43bd3ffb0a59278b65ff
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-16 01:25:43 +0300

    views, colors and frames, gradients, buttons and images, alerts

---
+++ Day 20/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mehmet Tekin on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
