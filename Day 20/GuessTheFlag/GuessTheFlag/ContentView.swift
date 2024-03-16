334a1f6422a5b530176aadb75eeb1af61b977696
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-17 02:07:54 +0300

    Stacking up buttons, alerts, stayling flags, upgrading the design

--- Day 20/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Day 20/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -1,25 +1,80 @@
-//
-//  ContentView.swift
-//  GuessTheFlag
-//
-//  Created by Mehmet Tekin on 14.03.2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
+        
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 246/255, green: 153/255, blue: 92/255), location: 0.3),
+                .init(color: Color(red: 81/255, green: 130/255, blue: 155/255), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+            .ignoresSafeArea()
+            
+            VStack {
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                
+                
+                VStack(spacing: 10) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.white)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .foregroundStyle(.secondary)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.ultraThinMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Text("Score: ???")
+                    .foregroundStyle(.white)
+                    .font(.title.bold().italic())
+            }
+            .padding()
+            
         }
-        .alert("Important Message", isPresented: $showingAlert){
-            Button("OK", role: .cancel) { }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
         } message: {
-            Text("Please read this.")
+            Text("Your score is ???")
         }
+        
     }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "False"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
 }
 
 #Preview {
