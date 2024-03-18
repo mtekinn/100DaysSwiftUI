47ac31d60580c841a0149f20a4dbc33a14481b90
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-19 02:45:52 +0300

    Milestone Project 1 Rock Paper Scissors initiak commit

---
+++ Day 25/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Mehmet Tekin on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
