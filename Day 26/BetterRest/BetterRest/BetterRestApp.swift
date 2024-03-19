a747b00adc95cf770a294c4bccd81fd67f7a0641
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-19 05:08:45 +0300

    BetterRest initial commit

---
+++ Day 26/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mehmet Tekin on 19.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
