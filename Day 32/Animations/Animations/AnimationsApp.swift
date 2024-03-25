3872586c625acc66d8905b0393011c55ce628d04
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-26 00:09:43 +0300

    Animations project initial commit

---
+++ Day 32/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Mehmet Tekin on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
