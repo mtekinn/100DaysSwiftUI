05d342119068ba3ea6ac260a0893b95ce1af84d2
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-27 02:20:11 +0300

    implicit animations, Customizing animations, Animating bindings, Creating explicit animations

--- Day 32/Animations/Animations/ContentView.swift
+++ Day 32/Animations/Animations/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap Me") {
+            withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                animationAmount += 360
+            }
         }
-        .padding()
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 1))
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 0, z: 1))
     }
 }
 
