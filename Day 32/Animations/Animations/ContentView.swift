1fe7a1560ea87eca5e332f3fc72008112a45ccf1
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-28 01:27:26 +0300

    animation stack

--- Day 32/Animations/Animations/ContentView.swift
+++ Day 32/Animations/Animations/ContentView.swift
@@ -8,21 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
-        Button("Tap Me") {
-            withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                animationAmount += 360
-            }
+        Button("Tap me") {
+            enabled.toggle()
         }
-        .padding(50)
-        .background(.red)
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
         .foregroundStyle(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 1, y: 1, z: 1))
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 0, z: 1))
+        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+        .animation(.default, value: enabled)
     }
 }
 
