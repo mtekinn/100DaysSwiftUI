4305ce9f3fefeae5457d6ad47c70029f0f7dfee6
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-29 00:28:36 +0300

    Controlling the animation stack, Animating gestures, Showing and hiding views with transitions, Building custom transitions using ViewModifier

--- Day 32/Animations/Animations/ContentView.swift
+++ Day 32/Animations/Animations/ContentView.swift
@@ -7,18 +7,47 @@
 
 import SwiftUI
 
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
 struct ContentView: View {
-    @State private var enabled = false
+    @State private var isShowingRed = false
     
     var body: some View {
-        Button("Tap me") {
-            enabled.toggle()
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .blue : .red)
-        .foregroundStyle(.white)
-        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
-        .animation(.default, value: enabled)
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
     }
 }
 
