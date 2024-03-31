9c7fc5e0a4b71c746b9eaa3b27b8d20f0da776a5
Muhammet Tekin <tekinmdev@gmail.com>
2024-03-31 04:37:50 +0300

    create class

---
+++ Day 36/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Mehmet Tekin on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
