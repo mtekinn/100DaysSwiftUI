9b4e736c72de9cd8f649512bc6b5e51ace5f26df
Muhammet Tekin <tekinmdev@gmail.com>
2024-04-11 00:36:56 +0300

    Resizing images, ScrollView, NavigationLink, Codable data, scrolling grid

--- Day 39/Moonshot/Moonshot/ContentView.swift
+++ Day 39/Moonshot/Moonshot/ContentView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let layout = [
+        GridItem(.adaptive(minimum: 80, maximum: 120)),
+        GridItem(.adaptive(minimum: 80, maximum: 120)),
+        GridItem(.adaptive(minimum: 80, maximum: 120))
+    ]
+    
     var body: some View {
-        Image(.howItShouldLook)
-            .resizable()
-            .scaledToFill()
-            .containerRelativeFrame(.horizontal) { size, axis in
-                size * 0.8
+        ScrollView {
+            LazyVGrid(columns: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
+                }
             }
+        }
     }
 }
 
