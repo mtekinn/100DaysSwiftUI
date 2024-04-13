cf74cfdd6335d1ff26773c11c8adec56ed30b03e
Muhammet Tekin <tekinmdev@gmail.com>
2024-04-14 01:30:08 +0300

    design UI

--- Day 39/Moonshot/Moonshot/Color-Theme.swift
+++ Day 39/Moonshot/Moonshot/Color-Theme.swift
@@ -5,4 +5,15 @@
 //  Created by Mehmet Tekin on 14.04.2024.
 //
 
-import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
+
