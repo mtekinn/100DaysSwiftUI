75f74b7be734f9f7d793bd55bb76ef4d79318b7d
Muhammet Tekin <tekinmdev@gmail.com>
2024-04-15 21:07:46 +0300

    missions detail

--- Day 39/Moonshot/Moonshot/MissionView.swift
+++ Day 39/Moonshot/Moonshot/MissionView.swift
@@ -5,4 +5,43 @@
 //  Created by Mehmet Tekin on 15.04.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                    .padding()
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
