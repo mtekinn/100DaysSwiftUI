add689a7186cee30fe3f2afcd8eefe814e06c430
Muhammet Tekin <tekinmdev@gmail.com>
2024-04-21 01:06:35 +0300

    wrap up commit

---
+++ Day 39/Moonshot/Moonshot/CrewMembersViews.swift
@@ -0,0 +1,49 @@
+//
+//  CrewMember.swift
+//  Moonshot
+//
+//  Created by Mehmet Tekin on 20.04.2024.
+//
+
+import SwiftUI
+
+struct CrewMembersViews: View {
+    let crew: [MissionView.CrewMember]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(crew, id: \.role) { crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 104, height: 72)
+                                .clipShape(.capsule)
+                                .overlay(
+                                    Capsule()
+                                        .strokeBorder(.white, lineWidth: 1)
+                                )
+                            
+                            VStack(alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .foregroundStyle(.white)
+                                    .font(.headline)
+                                
+                                Text(crewMember.role)
+                                    .foregroundStyle(.white.opacity(0.5))
+                            }
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CrewMembersViews(crew: [])
+}
