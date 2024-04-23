2ed349ef6aa864f90662aa7bcbfdfed1f092b900
Muhammet Tekin <tekinmdev@gmail.com>
2024-04-23 20:17:12 +0300

    Hashable

---
+++ Day 43/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Mehmet Tekin on 21.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
