cf183058e1f9e9cd09c630da495c7765330c46a8
Muhammet Tekin <tekinmdev@gmail.com>
2024-04-24 12:29:51 +0300

    programmatic navigation

---
+++ Day 43/Navigation/Navigation/DetailView.swift
@@ -0,0 +1,8 @@
+//
+//  DetailView.swift
+//  Navigation
+//
+//  Created by Mehmet Tekin on 24.04.2024.
+//
+
+import Foundation
---
+++ Day 43/Navigation/Navigation/PathStore.swift
@@ -0,0 +1,8 @@
+//
+//  PathStore.swift
+//  Navigation
+//
+//  Created by Mehmet Tekin on 24.04.2024.
+//
+
+import Foundation
