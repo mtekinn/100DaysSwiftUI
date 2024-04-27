60aa87b34b5bfab90600edd4910a69edeead968d
Muhammet Tekin <tekinmdev@gmail.com>
2024-04-27 16:52:25 +0300

    initial commit

---
+++ Day 47/HabitTracking/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Mehmet Tekin on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
