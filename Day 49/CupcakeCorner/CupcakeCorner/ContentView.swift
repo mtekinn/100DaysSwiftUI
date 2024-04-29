2313417f19ddb4d292e94cd9dfd4072758c65b13
Muhammet Tekin <tekinmdev@gmail.com>
2024-04-29 17:21:10 +0300

    initial commit

---
+++ Day 49/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Mehmet Tekin on 29.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
