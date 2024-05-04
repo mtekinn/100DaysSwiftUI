dfadfbfecd672600d4493db9bd62664c3ef8f9ff
Muhammet Tekin <tekinmdev@gmail.com>
2024-05-04 17:09:32 +0300

    image from a remote server. validating and disabling forms

--- Day 49/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Day 49/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,46 +7,28 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
     
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .task {
-            await loadData()
-        }
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=arctic+monkeys&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-        } catch {
-            print("Invalid data")
+            
+            Section {
+                Button("Create account") {
+                    print("Creating account...")
+                }
+            }
+            .disabled(username.isEmpty || username.isEmpty)
+            .disabled(disableForm)
         }
     }
 }
