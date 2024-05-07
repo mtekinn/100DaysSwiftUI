f57cac03c1272ddf3e3880458692fcde84b39b79
Muhammet Tekin <tekinmdev@gmail.com>
2024-05-07 13:06:33 +0300

    basic order details

--- Day 49/CupcakeCorner/CupcakeCorner/AddressView.swift
+++ Day 49/CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct AddressView: View {
+    var order: Order
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello World")
     }
 }
 
 #Preview {
-    AddressView()
+    AddressView(order: Order())
 }
+
--- Day 49/CupcakeCorner/CupcakeCorner/Order.swift
+++ Day 49/CupcakeCorner/CupcakeCorner/Order.swift
@@ -5,4 +5,23 @@
 //  Created by Mehmet Tekin on 7.05.2024.
 //
 
-import Foundation
+import SwiftUI
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
