7f7067ed45e68c8c6a125dc0fbebced40c421132
Muhammet Tekin <tekinmdev@gmail.com>
2024-05-24 01:53:15 +0300

    initial commit

---
+++ Day 53/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Mehmet Tekin on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
