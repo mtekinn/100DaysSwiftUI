afbf8288a938275b68cc07744bfbe61c2e253cde
Muhammet Tekin <tekinmdev@gmail.com>
2024-05-27 19:32:07 +0300

    project 11 some notes

---
+++ Day 53/Bookworm/Bookworm/PushButton.swift
@@ -0,0 +1,18 @@
+//
+//  PushButton.swift
+//  Bookworm
+//
+//  Created by Mehmet Tekin on 27.05.2024.
+//
+
+import SwiftUI
+
+struct PushButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PushButton()
+}
---
+++ Day 53/Bookworm/Bookworm/Student.swift
@@ -0,0 +1,8 @@
+//
+//  Student.swift
+//  Bookworm
+//
+//  Created by Mehmet Tekin on 27.05.2024.
+//
+
+import Foundation
