ab9c1eb1fa09cc6b7a9be2a0da1db98ada636632
Muhammet Tekin <tekinmdev@gmail.com>
2024-06-07 19:00:13 +0300

    showing book details, swiftData query

--- Day 53/Bookworm/Bookworm/ContentView.swift
+++ Day 53/Bookworm/Bookworm/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
+    @Query(sort: [
+        SortDescriptor(\Book.title),
+        SortDescriptor(\Book.author)
+    ]) var books: [Book]
+    // @Query(sort: \Book.rating, order: .reverse) var books: [Book]
     
     @State private var showingAddScreen = false
     
@@ -33,9 +37,18 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
+            .navigationDestination(for: Book.self) { book in
+                DetailView(book: book)
+            }
             .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
+                
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add Book", systemImage: "plus") {
                         showingAddScreen.toggle()
@@ -45,6 +58,16 @@ struct ContentView: View {
             .sheet(isPresented: $showingAddScreen) {
                 AddBookView()
             }
+        }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our query
+            let book = books[offset]
+            
+            // delete it from the context
+            modelContext.delete(book)
         }
     }
 }
