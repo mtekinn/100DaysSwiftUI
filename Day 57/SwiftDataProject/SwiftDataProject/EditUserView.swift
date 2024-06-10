cc9f6032d2a9c28e883d10a1b70c829d1e3fed00
Muhammet Tekin <tekinmdev@gmail.com>
2024-06-10 15:58:42 +0300

    day 57 commit

---
+++ Day 57/SwiftDataProject/SwiftDataProject/EditUserView.swift
@@ -0,0 +1,18 @@
+//
+//  EditUserView.swift
+//  SwiftDataProject
+//
+//  Created by Mehmet Tekin on 10.06.2024.
+//
+
+import SwiftUI
+
+struct EditUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditUserView()
+}
---
+++ Day 57/SwiftDataProject/SwiftDataProject/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  SwiftDataProject
+//
+//  Created by Mehmet Tekin on 10.06.2024.
+//
+
+import Foundation
