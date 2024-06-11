394675f8ddcebb5289a28e9121143f0f56448577
mtekinn <tekinmdev@gmail.com>
2024-06-11 18:27:42 +0300

    final commit

---
+++ Day 57/SwiftDataProject/SwiftDataProject/Job.swift
@@ -0,0 +1,8 @@
+//
+//  Job.swift
+//  SwiftDataProject
+//
+//  Created by Mehmet Tekin on 11.06.2024.
+//
+
+import Foundation
---
+++ Day 57/SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -0,0 +1,18 @@
+//
+//  UsersView.swift
+//  SwiftDataProject
+//
+//  Created by Mehmet Tekin on 11.06.2024.
+//
+
+import SwiftUI
+
+struct UsersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UsersView()
+}
