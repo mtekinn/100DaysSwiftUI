08c33c61708ee479f51b53cc999e24aa5bba0fe5
mtekinn <tekinmdev@gmail.com>
2024-06-18 00:29:27 +0300

    confirmationDialog commit

--- Day 62/Instafilter/Instafilter/ContentView.swift
+++ Day 62/Instafilter/Instafilter/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Hello, World!") {
+            showingConfirmation = true
         }
-        .padding()
+        .frame(width: 300, height: 300)
+        .background(backgroundColor)
+        .foregroundStyle(backgroundColor == .white ? .blue : .white)
+        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+            Button("Red") { backgroundColor = .red }
+            Button("Green") { backgroundColor = .green }
+            Button("Blue") { backgroundColor = .blue }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Select a new color")
+        }
     }
 }
 
