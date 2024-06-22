00595321628c4118b9b0a9401db8746df2880af0
mtekinn <tekinmdev@gmail.com>
2024-06-22 19:10:40 +0300

    Loading photos from the user's photo library

--- Day 62/Instafilter/Instafilter/ContentView.swift
+++ Day 62/Instafilter/Instafilter/ContentView.swift
@@ -6,19 +6,37 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
-    
+    @State private var pickerItem: PhotosPickerItem?
+    @State private var selectedImage: Image?
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
+
     var body: some View {
-        ContentUnavailableView {
-            Label("No snippets", systemImage: "swift")
-        } description: {
-            Text("You don't have any saved snippets yet.")
-        } actions: {
-            Button("Create Snippet") {
-                // create a snippet
+        VStack {
+            PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
+                Label("Select a picture", systemImage: "photo")
             }
-            .buttonStyle(.borderedProminent)
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+        }
+        .onChange(of: pickerItem) {
+            Task {
+                selectedImages.removeAll()
+                
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
         }
     }
     
